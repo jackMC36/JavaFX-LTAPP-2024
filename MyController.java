45ca21858ef31b51ecf61c72712bbcf8a05283b2
jackMC36 <154358389+jackMC36@users.noreply.github.com>
2024-02-14 15:22:09 +0100

    Modifications 14/02/2024
    Changed the UI, created a MyController class to control the actions for the button.
    
    Doesn't work yet, onAction="#handleButtonClick"  hasn't been implemented to the Button in FXML file. (Wasn't working).

---
+++ MyController.java
@@ -0,0 +1,9 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MyController {
+    @FXML
+    private void handleButtonClick(ActionEvent event) {
+        System.out.println("Killian");
+    }
+}
--- app.java
+++ app.java
@@ -2,26 +2,25 @@
 import java.io.IOException;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
  
+
 public class app extends Application {
+    
     public static void main(String[] args) {
         launch(args);
     }
+
     
     @Override
     public void start(Stage primaryStage) throws IOException {
 
         
         Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
-        primaryStage.setScene(new Scene(root, 1000, 750));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 }
