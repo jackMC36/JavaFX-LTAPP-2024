3158bf6708c497741379feb6ce61d0c8ea412626
jackMC36 <154358389+jackMC36@users.noreply.github.com>
2024-02-16 15:49:27 +0100

    Delete MyController.java

--- MyController.java
+++
@@ -1,9 +0,0 @@
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-
-public class MyController {
-    @FXML
-    private void handleButtonClick(ActionEvent event) {
-        System.out.println("Killian");
-    }
-}
