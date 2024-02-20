41cf00d708233f7f200c9c174d02718ba135e072
Jack <154358389+jackMC36@users.noreply.github.com>
2024-02-20 11:24:53 +0100

    PUSH : 20/02/2024
    Edited the GUI with Scene Builder, changed the overall style and added Controller Methods to Controler.java.

--- Controller.java
+++ Controller.java
@@ -1,13 +1,35 @@
 
-import javafx.event.ActionEvent;
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
 public class Controller {
-    @FXML
-    private void clickBouton(ActionEvent event) {
-        System.out.println("Killian");
-    }
-    
 
-    
+    @FXML
+    private ImageView slot;
+
+    @FXML
+    public void PlayWheel() throws InterruptedException {
+        RotateTransition rotate = new RotateTransition();
+        rotate.setNode(slot);
+        rotate.setDuration(Duration.millis(1000));
+        rotate.setInterpolator(Interpolator.LINEAR);
+        rotate.setCycleCount(TranslateTransition.INDEFINITE);
+        rotate.setByAngle(360);
+        rotate.play();
+    }
+
+    @FXML void SlowDown(){
+        RotateTransition rotate = new RotateTransition();
+        rotate.setNode(slot);
+        rotate.setDuration(Duration.millis(5000));
+        rotate.setInterpolator(Interpolator.LINEAR);
+        rotate.setCycleCount(TranslateTransition.INDEFINITE);
+        rotate.setByAngle(360);
+        rotate.play();
+    }
+   
 }
--- app.java
+++ app.java
@@ -1,11 +1,16 @@
  
 import java.io.IOException;
 
+import org.w3c.dom.events.Event;
+
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
  
 
@@ -14,21 +19,36 @@ public class app extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
     
     @Override
     public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("app.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
 
-        
-        Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add("style.css");
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+
+                    case SPACE: System.out.println("SPACE");;
+                        break;
+                
+                    default: System.out.println("Wrong Key");
+                        break;
+                }            }
+            
+        });
+
         primaryStage.setScene(scene);
         primaryStage.setTitle("BDE Adess");
         Image icon = new Image("logo.png");
-        primaryStage.setFullScreen(true);
-        primaryStage.setFullScreenExitHint("Appuyez sur échap pour sortir");
         primaryStage.getIcons().add(icon);
+
+
         primaryStage.show();
     }
 }
