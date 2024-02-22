c7fd9ed893c30f2bf3a86d08f309771715ed4905
Jack <154358389+jackMC36@users.noreply.github.com>
2024-02-22 14:24:27 +0100

    PUSH: 22/02/2024

--- Controller.java
+++ Controller.java
@@ -1,4 +1,5 @@
 
+import javafx.animation.FadeTransition;
 import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
@@ -6,6 +7,7 @@ import javafx.animation.RotateTransition;
 import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.util.Duration;
 
@@ -13,7 +15,13 @@ public class Controller {
 
     @FXML
     private ImageView slot;
+    @FXML
+    private ImageView Win;
+    @FXML
+    private ImageView WinText;
 
+    Image WinImage = new Image(getClass().getResourceAsStream("pika.jpeg"));
+    Image WinTextImage = new Image(getClass().getResourceAsStream("WinText.png"));
 
     private RotateTransition rotate;
 
@@ -34,8 +42,37 @@ public class Controller {
             new KeyFrame(Duration.seconds(0), new KeyValue(rotate.rateProperty(), 1)),
             new KeyFrame(Duration.seconds(3), new KeyValue(rotate.rateProperty(), 0))
         );
+        timeline.setOnFinished(event -> {
+            WinImage();
+            WinText();
+        });
         timeline.play();
-
     }
-   
+
+    public void WinImage(){
+        Win.setImage(WinImage);
+        Win.setOpacity(0);
+        Win.setVisible(true);
+        FadeTransition fade = new FadeTransition();
+        fade.setNode(Win);
+        fade.setFromValue(0);
+        fade.setToValue(1);
+        fade.setDuration(Duration.seconds(3));
+        fade.play();
+    }
+    
+
+    public void WinText() {
+        WinText.setImage(WinTextImage);
+        WinText.setOpacity(0);
+        WinText.setVisible(true);
+
+        FadeTransition fade = new FadeTransition();
+        fade.setNode(WinText);
+        fade.setFromValue(0);
+        fade.setToValue(1);
+        fade.setDuration(Duration.seconds(3));
+        fade.play();
+    }
+    
 }
